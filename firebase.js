f3782f7d6194e6081d03e6605cb01d4102d532a1
francescoplacidi87-source <francescoplacidi87@gmail.com>
2026-07-31 11:15:59 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2bwlk4KlCjdKzmMkGhF-ydNMGwK_C1Fw",
+  authDomain: "sfida-parole.firebaseapp.com",
+  projectId: "sfida-parole",
+  storageBucket: "sfida-parole.firebasestorage.app",
+  messagingSenderId: "786532097924",
+  appId: "1:786532097924:web:b98f037451ebf165f987d1"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
